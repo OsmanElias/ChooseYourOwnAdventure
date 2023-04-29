d2a285da8ab81c9924578f471a8373ab8deb53d6
OsmanElias <93014594+OsmanElias@users.noreply.github.com>
2023-04-29 14:17:05 -0400

    Signup/Login fix
    Edits to get the database to insert data, fixed login and signup routes.

--- src/index.js
+++ src/index.js
@@ -1,57 +1,54 @@
-const express = require ("express") 
-const app = express()
-const path = require("path")
-const hbs = require("hbs") //for hbs files
-const collection = require("./mongodb")
+const express = require("express");
+const app = express();
+const path = require("path");
+const hbs = require("hbs"); //for hbs files
+const collection = require("./mongodb");
 
+const tempelatePath = path.join(__dirname, "./templates");
 
+app.use(express.json()); //To get hbs files
+app.set("view engine", "hbs"); //Our view engine will be hbs
+app.set("views", tempelatePath); //For the looks
+app.use(express.urlencoded({ extended: false })); //to get the code to run
+app.use(express.static(path.join(__dirname, 'public'))); // Serve static files
 
-const templatePath = path.join(__dirname, '../templates');
+app.get("/", (req, res) => {
+    res.render("login");
+  });
+  
+app.get("/signup", (req, res) => {
+  res.render("signup");
+});
 
+app.post("/signup", async (req, res) => {
+  const data = {
+    name: req.body.name,
+    password: req.body.password,
+  };
 
+  try {
+    await collection.create(data);
+    res.render("home");
+  } catch (error) {
+    console.error("Error inserting data:", error);
+    res.status(500).send("Error inserting data");
+  }
+});
 
-app.use(express.json()) //To get hbs files
-app.set("view engine", "hbs") //Our view engine will be hbs
-app.set("views",templatePath) //For the looks
-app.use(express.urlencoded({extended:false}))
+app.post("/login", async (req, res) => {
+  try {
+    const check = await collection.findOne({ name: req.body.name }); //checking if user exists
 
-app.get("/",(req,res)=>{     //Get for login this is the homepage
-    res.render("login")
-
-
-})
-
-app.get("/signup",(req,res)=>{       //Get for signup
-    res.render("signup")
-    
-
-})
-
-
-
-app.post("/signup",async (req,res)=>{ //Working with async here
-
-
-    const data={
-        name:req.body.name,          //Grabbing information from signup hbs
-        password:req.body.password
+    if (check.password === req.body.password) {
+      res.render("home");
+    } else {
+      res.send("Password is incorrect");
     }
+  } catch {
+    res.send("Error");
+  }
+});
 
-    await collection.insertMany([data]) //fills collection 
-    
-    res.render("home")
-
-
-})
-
-
-
-
-
-
-app.listen(3000,()=>{         //For connection
- 
-    console.log("Connection made");
-})
-
-
+app.listen(3000, () => {
+  console.log("Connection made");
+});
--- src/mongodb.js
+++ src/mongodb.js
@@ -1,27 +1,30 @@
-const mongoose = require ("mongoose")
+const mongoose = require("mongoose")
 
-mongoose.connect("mongodb://localhost:27017/ChooseAdventure") // Connecting to Mongo and to our database
-.then(()=>{
-    console.log("Connected to database!");
-})
-.catch(()=>{
-    console.log("Error no connection");
-})
+const connectionOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}
 
+mongoose.connect("mongodb://127.0.0.1:27017/ChooseAdventure", connectionOptions)
+
+const db = mongoose.connection
+
+db.on("error", console.error.bind(console, "Error connecting to MongoDB:"))
+db.once("open", () => {
+  console.log("Connected to MongoDB!")
+})
 
 const LogInSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
-    },
-    password:{
-        type:String,
-        required:true
-    }
+  name: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 })
 
-
-const collection = new mongoose.model("CollectionOne",LogInSchema)
-
+const collection = mongoose.model("CollectionOne", LogInSchema)
 
 module.exports = collection
